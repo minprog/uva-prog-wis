1cabdb9fb8b9d218c7a96c5582c6bcffe857915e
André Heck <a.j.p.heck@uva.nl>
2016-02-22 14:49:11 +0100

    Added files via upload

--- 40 Week 4/10 Problem Set 5/ps5_template.py
+++ 40 Week 4/10 Problem Set 5/ps5_template.py
@@ -16,7 +16,7 @@ def substring_match_one_sub(key,target):
         # key1 and key2 are substrings to match
         key1 = key[:miss]
         key2 = key[miss+1:]
-        print 'breaking key',key,'into',key1,key2
+        print('breaking key',key,'into',key1,key2)
         # match1 and match2 are tuples of locations of start of matches
         # for each substring in target
         match1 = substring_match_exact(target,key1)
@@ -25,7 +25,7 @@ def substring_match_one_sub(key,target):
         # need to filter pairs to decide which are correct
         filtered = constrained_match_pair(match1,match2,len(key1))
         all_answers = all_answers + filtered
-        print 'match1',match1
-        print 'match2',match2
-        print 'possible matches for',key1,key2,'start at',filtered
+        print('match1',match1)
+        print('match2',match2)
+        print('possible matches for',key1,key2,'start at',filtered)
     return all_answers
